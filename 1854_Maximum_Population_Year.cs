4c022c7ec70d9124b3d4c0e19de94115659de9b4
Bonjarva <jaydin.m@hotmail.com>
2023-10-22 14:16:53 +0200

    22/10/23 commented plan for solution

--- 1854_Maximum_Population_Year.cs
+++ 1854_Maximum_Population_Year.cs
@@ -33,10 +33,17 @@ public class Solution
     public int MaximumPopulation(int[][] logs)
     {
 
+        //Create an array for each of the 100 years for the population constraints.
+        //iterate through each of the logs
+        //  add 1 to each of the array positions for each year they were alive
+
+        //the position + 1950 will equal the year specified
+
+        //keep track of the largest month ad only update the year if we find a larger month
+        //  we want the earliest so if they are equal then we dont want that to overwrite the previous year.
 
 
-
-
+        //return the largest month we found
     }
 }
 
