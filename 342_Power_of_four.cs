7fad634ce3f396a9e637fe16405c0f27b7e7ea32
Bonjarva <jaydin.m@hotmail.com>
2023-10-23 18:01:48 +0200

    23/10/23 challenge initial sete up and code plan

---
+++ 342_Power_of_four.cs
@@ -0,0 +1,38 @@
+/*
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ 
+
+Example 1:
+
+Input: n = 16
+Output: true
+Example 2:
+
+Input: n = 5
+Output: false
+Example 3:
+
+Input: n = 1
+Output: true
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+*/
+
+public class Solution
+{
+    public bool IsPowerOfFour(int n)
+    {
+        //Iterate through each of the powers of 4 until we find a match
+        // if we overshoot our n value return false
+        // if we find a match return true;
+
+
+
+    }
+}
