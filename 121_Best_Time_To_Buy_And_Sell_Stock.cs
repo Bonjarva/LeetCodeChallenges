324a8edd684b56ff7ed2b2f1ad6f4a59e924bc81
Jaydin McMullan <jaydin.m@hotmail.com>
2025-04-14 12:19:56 +0200

    New solution with O(n) as we store lowest price as we go instead of iterating through twice. Passes all tests

--- 121_Best_Time_To_Buy_And_Sell_Stock.cs
+++ 121_Best_Time_To_Buy_And_Sell_Stock.cs
@@ -1,48 +1,22 @@
-/*
-You are given an array prices where prices[i] is the price of a given stock on the ith day.
-
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-
-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-
- 
-
-Example 1:
-
-Input: prices = [7,1,5,3,6,4]
-Output: 5
-Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
-Example 2:
-
-Input: prices = [7,6,4,3,1]
-Output: 0
-Explanation: In this case, no transactions are done and the max profit = 0.
- 
-
-Constraints:
-
-1 <= prices.length <= 105
-0 <= prices[i] <= 104
-*/
-
 public class Solution {
     public int MaxProfit(int[] prices) {
         
         // Stored variable for max price 
         int profit = 0;
+        int lowestPrice = prices[0];
 
+        for (int i = 1; i < prices.Length; i++){
+            // check if this is the lowest price we've seen
+            if (prices[i] < lowestPrice){
+                lowestPrice = prices[i];
+                continue;
+            }
 
-        //two pointers, one for buying the stock, one for selling
-        
-        for (int i = 0; i < prices.Length -1 ; i++){
-            for (int j = i+1; j < prices.Length; j++){
-                if (prices[j]-prices[i] > profit){
-                    profit = prices[j]-prices[i];
-                }
+            // check for potential profit of current index to lowest price
+            if (prices[i]-lowestPrice > profit){
+                profit = prices[i]-lowestPrice;
             }
         }
-    
 
         return profit;
     }
